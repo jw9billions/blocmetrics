4c8b99eaa03af71e30307b79f5a061e272b860b4
Jane Wang <jw9billions@gmail.com>
2017-11-20 19:24:58 -0800

    cp5 add Events in Apps

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+test1 = User.create!(
+  email: 'test1@test.com',
+  password: 'password'
+)
+
+test2 = User.create!(
+  email: 'test2@test.com',
+  password: 'password'
+)
+
+test3 = User.create!(
+  email: 'test3@test.com',
+  password: 'password'
+)
+
+users = User.all
+
+10.times do
+  RegisteredApplication.create!(
+    name: Faker::App.name,
+    url: Faker::Internet.url,
+    user: users.sample
+  )
+end
+
+registered_applications = RegisteredApplication.all
+
+#sample events
+event_list = ['eat', 'drink', 'sleep', 'six']
+
+100.times do
+  Event.create!(
+    name: event_list.sample,
+    registered_application: registered_applications.sample
+  )
+end
+
+events = Event.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{RegisteredApplication.count} registered applications created"
+puts "#{Event.count} events created"
